f8f50177c7817f7b2823eba83172fe1d92d4b181
Tom Viner <tom@viner.tv>
2020-09-08 22:32:54 +0100

    format code

--- gsxr-dash/code.c
+++ gsxr-dash/code.c
@@ -1,27 +1,29 @@
 /*
-* Copyright 2013 Aaron Turner
-*
-* Wrote this code to make it easy to built the temperature ADC map
-* since the SV650 ECU has the ADC and it's up to the dash to convert
-* that into degrees F/C. I used this with the OEM dash to figure out
-* what ADC values mapped to what temperatures.
-*
-* To support 7800 baud, you have to hack your SoftwareSerial.cpp file
-* and add this to the 16Mhz struct:
-* { 7800, 138, 291, 291, 287, },
-*
-*
-* Update By Blagger (www.Kill-switch.co.ukForum)
-*
-* Removed and edited some of the base code for GSXR Dash support.
-* Removed temperature change by buttons and added analog input from
-* a thermistor connected to Analog Port 0.
-* Changed ECU Pin to Digital Pin 5.
-* Still supports Serial monitor (115200 baud)
-*
-*/
+ * Copyright 2013 Aaron Turner
+ *
+ * Wrote this code to make it easy to built the temperature ADC map
+ * since the SV650 ECU has the ADC and it's up to the dash to convert
+ * that into degrees F/C. I used this with the OEM dash to figure out
+ * what ADC values mapped to what temperatures.
+ *
+ * To support 7800 baud, you have to hack your SoftwareSerial.cpp file
+ * and add this to the 16Mhz struct:
+ * { 7800, 138, 291, 291, 287, },
+ *
+ *
+ * Update By Blagger (www.Kill-switch.co.ukForum)
+ *
+ * Removed and edited some of the base code for GSXR Dash support.
+ * Removed temperature change by buttons and added analog input from
+ * a thermistor connected to Analog Port 0.
+ * Changed ECU Pin to Digital Pin 5.
+ * Still supports Serial monitor (115200 baud)
+ *
+ */
 #include <SoftwareSerial.h>
+
 #include <PT6961.h>
+
 #define CS 10
 #define MOSI 11
 #define CLK 13
@@ -41,102 +43,100 @@ int new_temp;
 SoftwareSerial ecu(RX, TX);
 PT6961 LED(MOSI, CLK, CS);
 
-
 void
-clear_buf()
-{
-int j;
-for (j = 0; j < 7; j++) {
-sbytes[j] = 0;
-}
+clear_buf() {
+  int j;
+  for (j = 0; j < 7; j++) {
+    sbytes[j] = 0;
+  }
 }
 
 void setup() {
-Serial.begin(115200);
-Serial.print("Starting\n
-LED.initDisplay();
-ecu.begin(ECU_SPEED);
-clear_buf();
-calc_csum();
-sensor = 0; //set which pin thermistor is connected too
-current_temp = analogRead(sensor);
+  Serial.begin(115200);
+  Serial.print("Starting\n");
+  LED.initDisplay();
+  ecu.begin(ECU_SPEED);
+  clear_buf();
+  calc_csum();
+  sensor = 0; //set which pin thermistor is connected too
+  current_temp = analogRead(sensor);
 }
 
 /*
-* calculates the checksum of sbytes[]
-*/
+ * calculates the checksum of sbytes[]
+ */
 void
 calc_csum() {
-byte check_sum = 0;
+  byte check_sum = 0;
 
-// Calculate check sum byte
-for (int x = 0; x <= 6; x++) {
-check_sum = check_sum + sbytes[x];
-}
+  // Calculate check sum byte
+  for (int x = 0; x <= 6; x++) {
+    check_sum = check_sum + sbytes[x];
+  }
 
-sbytes[7] = 256 - check_sum;
+  sbytes[7] = 256 - check_sum;
 }
 
 // printf to the HW serial port
 void
-serial_printf(char *fmt, ... ) {
-char tmp[128]; // resulting string limited to 128 chars
-va_list args;
-va_start (args, fmt );
-vsnprintf(tmp, 128, fmt, args);
-va_end (args);
-Serial.print(tmp);
+serial_printf(char * fmt, ...) {
+  char tmp[128]; // resulting string limited to 128 chars
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(tmp, 128, fmt, args);
+  va_end(args);
+  Serial.print(tmp);
 }
 
 // Read temperature from thermistor
 unsigned int
-change_temp()
-{
-unsigned int temp = 0;
-int sense = 0;
+change_temp() {
+  unsigned int temp = 0;
+  int sense = 0;
 
-temp = sbytes[0] << 2;
-temp += (sbytes[1] & 0xc0) >> 6;
-sense= analogRead(sensor);
-temp = sense;
-sbytes[0] = temp >> 2;
-sbytes[1] = temp << 6;
+  temp = sbytes[0] << 2;
+  temp += (sbytes[1] & 0xc0) >> 6;
+  sense = analogRead(sensor);
+  temp = sense;
+  sbytes[0] = temp >> 2;
+  sbytes[1] = temp << 6;
 
-serial_printf("temp = %04u\n", temp);
-calc_csum();
-new_temp = temp;
-return temp;
+  serial_printf("temp = %04u\n", temp);
+  calc_csum();
+  new_temp = temp;
+  return temp;
 }
-
 
 void
-print_temp(unsigned int temp)
-{
-int i = 0;
-byte val;
-byte led_digits[4] = { 0xc0, 0xc2, 0xc4, 0xc6 };
-LED.sendNum((int)temp, 0);
+print_temp(unsigned int temp) {
+  int i = 0;
+  byte val;
+  byte led_digits[4] = {
+    0xc0,
+    0xc2,
+    0xc4,
+    0xc6
+  };
+  LED.sendNum((int) temp, 0);
 }
 
-void loop()
-{
-int x;
-unsigned int temp;
+void loop() {
+  int x;
+  unsigned int temp;
 
-// Write data bytes to serial
-for (x = 0; x <= 6; x++) {
-ecu.write(sbytes[x]);
-delay(10);
-}
-ecu.write(sbytes[7]);
+  // Write data bytes to serial
+  for (x = 0; x <= 6; x++) {
+    ecu.write(sbytes[x]);
+    delay(10);
+  }
+  ecu.write(sbytes[7]);
 
-// Intermessage delay
-delay(60);
+  // Intermessage delay
+  delay(60);
 
-
-temp = change_temp();
-print_temp(temp);
-serial_printf("%02x %02x %02x %02x %02x %02x %02x %02x\n",
-sbytes[0], sbytes[1], sbytes[2], sbytes[3],
-sbytes[4], sbytes[5], sbytes[6], sbytes[7]);
+  temp = change_temp();
+  print_temp(temp);
+  serial_printf("%02x %02x %02x %02x %02x %02x %02x %02x\n",
+    sbytes[0], sbytes[1], sbytes[2], sbytes[3],
+    sbytes[4], sbytes[5], sbytes[6], sbytes[7]);
 }
